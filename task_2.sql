c5bee564fb08e56dc23b49d304812d7c67c6f963
Sławomir Żurek <94127146+slawomirzurek@users.noreply.github.com>
2022-04-23 14:52:33 +0200

    Formatted the file

--- task_2.sql
+++ task_2.sql
@@ -1,24 +1,24 @@
-CREATE TABLE ZADANIE_2 AS SELECT * FROM(
+CREATE TABLE zadanie_2 AS SELECT * FROM(
 SELECT
     regexp_substr(tekst, '^[^.]*')*1 AS ID,
-    regexp_substr(tekst, '\w+\s\S*') AS DZIEN,
-    regexp_substr(tekst, '\d{4}')*1 AS ROK,
-    TRIM(regexp_replace(regexp_substr(tekst, '([0-9]{4}\s)[A-Z]+[\s|-]{0,1}[A-Z]+[^K-]'),'([0-9]{4}\s)', '')) AS SKOCZNIA,
-    regexp_replace(regexp_substr(tekst, '[-]\d+'), '-', '')*1 AS PUNKT_K,
+    regexp_substr(tekst, '\w+\s\S*') AS dzien,
+    regexp_substr(tekst, '\d{4}')*1 AS rok,
+    TRIM(regexp_replace(regexp_substr(tekst, '([0-9]{4}\s)[A-Z]+[\s|-]{0,1}[A-Z]+[^K-]'),'([0-9]{4}\s)', '')) AS skocznia,
+    regexp_replace(regexp_substr(tekst, '[-]\d+'), '-', '')*1 AS punkt_k,
     CASE
-        WHEN tekst LIKE '%HS-%' THEN replace(regexp_substr(tekst,'(HS-)[0-9]+'),'HS-','')*1
-        ELSE Null
-    END HILL_SIZE,
-    regexp_substr(tekst, '[0-9]+[,][0-9]')*1 AS SKOK_1,
+        WHEN tekst LIKE '%HS-%' THEN REPLACE(regexp_substr(tekst,'(HS-)[0-9]+'),'HS-','')*1
+        ELSE NULL
+    END hill_size,
+    regexp_substr(tekst, '[0-9]+[,][0-9]')*1 AS skok_1,
     CASE
         WHEN REGEXP_LIKE (tekst, '[m]\s[–]') THEN NULL
-        ELSE regexp_replace(regexp_substr(tekst, '[m]\s[0-9]+[,][0-9]\s[m]'), '[m\s]', '')*1
-    END SKOK_2,
-    regexp_replace(regexp_substr(tekst, '[0-9]+[,][0-9]\s(pkt)\s\d'), '\s(pkt)\s[0-9]', '')*1 AS NOTA,
-    TRIM(replace(regexp_substr(tekst, '(\s\d\.)'), '.',''))*1 AS LOKATA,
+        ELSE (regexp_replace(regexp_substr(tekst, '[m]\s[0-9]+[,][0-9]\s[m]'), '[m\s]', ''))*1
+    END skok_2,
+    regexp_replace(regexp_substr(tekst, '[0-9]+[,][0-9]\s(pkt)\s\d'), '\s(pkt)\s[0-9]', '')*1 AS nota,
+    TRIM(REPLACE(regexp_substr(tekst, '(\s\d\.)'), '.',''))*1 AS lokata,
     CASE
-        WHEN REGEXP_LIKE (tekst, '(\d\.\s\W)') THEN Null
+        WHEN REGEXP_LIKE (tekst, '(\d\.\s\W)') THEN NULL
         ELSE regexp_replace(regexp_substr(tekst, '(\s\d\.\s[0-9]+,[0-9]+)'),('\s\d\.\s'),'')*1
-    END STRATA,
-    replace(replace(substr(tekst, instr(tekst,'pkt ',-1)), 'pkt ', ''),'1. –', '') AS ZWYCIEZCA
-FROM DW.SKOKI);
+    END strata,
+    REPLACE(REPLACE(substr(tekst, instr(tekst,'pkt ',-1)), 'pkt ', ''),'1. –', '') AS zwyciezca
+FROM dw.skoki);
